06112e831a790c867c640e240b406fb09c77a693
Unknown <anthony.d.bardsley@gmail.com>
2019-02-19 10:28:53 -0700

    debugging
    added bottom bracket, but more errors appear

--- ChooseYourOwnAdventure/game.js
+++ ChooseYourOwnAdventure/game.js
@@ -15,25 +15,21 @@ Game();
 
 
 function Game(){
-    var woops = ("I didnt understand. would you repeat?");
+    var woops = alert("I didnt understand. would you repeat?");
     var inventory = {
         sword: 0,
         lamp: 0,
         rope: 0,
         loot: 0,
     }
-    var fight{
-        if (sword === 1){
-            
-        }
-        else{
-            
-        }
+    
+    var checkLoot = function(){
+        alert ("This is how much you got" + inventory.loot);
     }
     
     var wizName = ["jimmy, dolf, brian, theo"];
     
-    alert("legend of " +wizName[Math.floor(Math.random(0,4)] + "!");
+    alert("legend of " +wizName[ Math.floor( Math.random(0,4) ) ]+ "!");
         
     document.write("the unforuntae tale of ");
                                 
@@ -48,8 +44,9 @@ function Game(){
         if( prison === "look around" || prison === "look"){
             var prisonLook = prompt("The Prison is small, dirty, and damp. A rat curries into a hole in the corner. There is a barred window to the back. a straw bed to the right. The cell bars and locked door to the front. \n move bed \n pick lock").toLocaleLowerCase();
             //move bed
-                                if prisonLook === "move" || prisonLook === "move bed"{
+                                if (prisonLook === "move" || prisonLook === "move bed"){
                                     alert ("after a small a few moments of pushing the bed moves and there apears to be a tunnel");
+                                tunnels();
                                     
                                 }
                 //tunnels
@@ -74,43 +71,60 @@ function Game(){
         }
     }                        //prison tunnels
                                 
-                                function tunnels() {
-                                   var enterTunnel = prompt("would you like to go in? \n yes \n no").toLocaleLowerCase();
-                                if enterTunnel === "yes" || enterTunnel === "y" {
-                                // the tunnel system begins
-                                    var tunnel = prompt ("");
-                                }
-                                else enterTunnel === "no" || enterTunnel === "n" {
-                                 alert (" you slide the bed back");
-                                Prison();
-                                }
-                                }
+        
+    function tunnels() {
+                                 
+        var enterTunnel = prompt("would you like to go in? \n yes \n no").toLocaleLowerCase();
+                                
+               
+        if (enterTunnel === "yes" || enterTunnel === "y") {
+                                
+            // the tunnel system begins
+                   
+            var tunnel = prompt ("");
+                                       
+        }
+            
+        else ( enterTunnel === "no" || enterTunnel === "n" )
+             {
+                                 
+                 alert (" you slide the bed back");
+                                
+                 Prison();                
+               
+             }
+    
+    }
 
 //next part 
-    function Swamp() {
+    function Swamp() 
+    {
         var swampEnv = prompt ("you got out and you are now in a swamp. There is a path to north, a murky pond to the east, and the prison is to the south. \n follow path north \n swim").toLocaleLowerCase();
         
         //follow path
-        if(swampEnv == "follow" ||swampEnv == "follow path"|| swampEnv == "north"){
+        if(swampEnv == "follow" ||swampEnv == "follow path"|| swampEnv == "north")
+        {
             var hut = true;
             var swampPath = prompt ("You follow the path to the north. along the way you notice a hut with a light buring inside. \n enter hut \n burn down hut \n continue along path").toLocaleLowerCase;
             
-            switch(swampPath){
+            switch(swampPath)
+            {
 
                     //burn hut
-                case "burn"||"burn down hut":
+                case "burn" || "burn down hut":
                      
-                    alert("the hut is quickly engulfed in flames. You hear a deathening shreak  and a smell of buring flesh begins to eminate from inside of the hut. You glory at the flames as they lick the walls of the hut and everything is quickly turned to cinder and smoke.").toLowerCase();
+                    alert("the hut is quickly engulfed in flames. You hear a deathening shreak  and a smell of buring flesh begins to eminate from inside of the hut. You glory at the flames as they lick the walls of the hut and everything is quickly turned to cinder and smoke.");
                       // no more hut
                     var hut = false;
                     break;
                     
                     //go in hut
-                case "enter" && hut = true:
+                case "enter" && hut === true:
                 
                     var hutSpace = prompt ("you go inside the hut and you find an old hag who is quietly mumbling to herself over a book. You notice a sword to the left \n talk with her \n take sword \n leave hut").toLocaleLowerCase();
                     //take the sword
-                    if(hutSpace === "take sword" || hutSpace === "take"){
+                    if(hutSpace === "take sword" || hutSpace === "take")
+                    {
                     
                         inventory.sword ++;
                   
@@ -121,11 +135,13 @@ function Game(){
                                 // retry
                                     var hutRetry = prompt ("would you like to try again? \n yes").toLowerCase();
                                 
-                                    if (hutRetry == "yes"|| hutRetry == "y"){
+                                    if (hutRetry == "yes"|| hutRetry == "y")
+                                    {
                                         Swamp();
                                     }
                                 
-                                    else{
+                                    else
+                                    {
                                         EndGame();
                                 
                                     }
@@ -134,15 +150,16 @@ function Game(){
                                 }
                     }
                    break;
+                    
                     //follow path
                 case "continue" || "path" || "continue along path":
                     alert("you decided to just leave the hut and continue on the path. Cool");
-                    var road
+                    var road = prompt("the path goes on for a bit but now there is a split \n ").toLocaleLowerCase();
                     break;
                                 
                 default:
                                 //miss type so retry
-                            alert ( woops ) ;    
+                            woops();    
                             
                    
                 break;
@@ -150,5 +167,8 @@ function Game(){
                                 
             }
             //inside the hut
+   
+        }
+
     }
 }
